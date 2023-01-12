77e8f4ce729c9359ef188a5bc8225ec30ea04b66
Dzirdonovich <zxxz3.1996@list.ru>
2023-01-13 01:31:42 +0300

    1v

--- cashierServer/src/main.ts
+++ cashierServer/src/main.ts
@@ -3,9 +3,9 @@ import { AppModule } from './app.module';
 import cors from 'cors';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors: true});
   app.setGlobalPrefix('api/v1');
-  app.enableCors();
+
   await app.listen(4000);
 }
 bootstrap();
