946910aac54aa456c002e858d9d0f02edb9d7a3f
Dzirdonovich <zxxz3.1996@list.ru>
2023-01-12 23:12:19 +0300

    1v

--- cashierServer/src/core/database/database.config.ts
+++ cashierServer/src/core/database/database.config.ts
@@ -6,9 +6,9 @@ dotenv.config();
 export const databaseConfig: IDatabaseConfig = {
     development: {
         username: process.env.DB_USER,
-        password: process.env.DB_PASS,
+        password: process.env.DB_PASS_PRODUCTION,
         database: process.env.DB_NAME_DEVELOPMENT,
-        host: process.env.DB_HOST,
+        host: process.env.DB_HOST_PRODUCTION,
         port: process.env.DB_PORT,
         dialect: process.env.DB_DIALECT,
     },
