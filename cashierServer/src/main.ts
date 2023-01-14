75da2711a9b7d2b881645b2ebe066d1bfcde1f5f
Dzirdonovich <zxxz3.1996@list.ru>
2023-01-14 23:30:53 +0300

    1v

--- cashierServer/src/main.ts
+++ cashierServer/src/main.ts
@@ -5,7 +5,7 @@ import cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
-  app.enableCors({origin: 'https://cashier-v2-qrsj.vercel.app/'});
+  app.enableCors({origin: 'https://app.cashierdodonow.space/'});
   await app.listen(4000);
 }
 bootstrap();
