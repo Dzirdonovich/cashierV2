8bb26cc34079bfe3a9b06e55ae54a1cdee260211
Dzirdonovich <zxxz3.1996@list.ru>
2023-01-18 12:57:34 +0300

    1v

--- cashierClient/src/store/asyncThynk/HTTPMenu.ts
+++ cashierClient/src/store/asyncThynk/HTTPMenu.ts
@@ -2,6 +2,13 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const getMenu: any = createAsyncThunk("menus/getMenu", async () => {
-  const { data } = await axios.get(`${process.env.REACT_APP_PRODUCTION_HOST || "localhost"}/api/v1/menu-item`,);
+  const { data } = await axios.get(
+    `${process.env.REACT_APP_PRODUCTION_HOST || "localhost"}/api/v1/menu-item`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
   return data;
 });
--- cashierServer/src/main.ts
+++ cashierServer/src/main.ts
@@ -4,9 +4,21 @@ import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({origin: ['https://cashier-v2-qrsj.vercel.app', "http://cashier-v2-qrsj.vercel.app"]});
-
+  app.enableCors();
   app.setGlobalPrefix('api/v1');
-  app.enableCors();await app.listen(4000);
+  app.use(function (req, res, next) {
+    res.header(
+      'Access-Control-Allow-Origin',
+      'https://cashier-v2-qrsj.vercel.app/',
+    );
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept',
+    );
+    console.log(1);
+    next();
+  });
+  console.log(2);
+  await app.listen(4000);
 }
 bootstrap();
